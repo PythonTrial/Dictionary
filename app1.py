d47ffab895b1954d33cf2a3d397dd11dbf0a2fe6
unknown <machel.s.kassah@gmail.com>
2020-01-25 16:29:01 +0000

    checked for wrong input from user

--- app1.py
+++ app1.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -6,8 +7,17 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys()))>0:
+        response = input("Did you mean %s instead? Enter Y for Yes, or N for No:  " % get_close_matches(w, data.keys())[0])
+        if response.upper() == "Y":
+            return data[get_close_matches(w,data.keys())[0]]
+        elif response.upper() == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "Entry not identified. Try again"
     else:
         return "The word doesn't exist. Please double check it."
+
 
 word = input("Enter word: ")
 
